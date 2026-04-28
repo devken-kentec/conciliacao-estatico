8f7c6c5e9c2aa963a7a2b230c319cae4aaf19c7f
Kennedy Tomazete <kennedy_tomazete@outlook.com>
2026-04-28 16:57:21 -0300

    'extratos'

--- src/app/modulos/extrato/extrato-list/extrato-list.component.ts
+++ src/app/modulos/extrato/extrato-list/extrato-list.component.ts
@@ -3,10 +3,16 @@ import { Component, inject } from '@angular/core';
 import { ExtratoService } from '../extrato.service';
 import { take, pipe } from 'rxjs';
 import { Extrato } from '../../../domain/extrato.domain';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-extrato-list',
-  imports: [],
+  imports: [
+        ReactiveFormsModule,
+        FormsModule,
+        [NgStyle]
+  ],
   templateUrl: './extrato-list.component.html',
   styleUrl: './extrato-list.component.css'
 })
@@ -18,18 +24,86 @@ export class ExtratoListComponent {
     public banco!: string;
     public agencia!: string;
     public conta!: string;
+    private fb = inject(FormBuilder);
+    public  paginaForm!: FormGroup;
+    public totalElements = 0;
+    public totalPages = 0;
+    public pagina = 0;
+    public tamanho = 50;
+    public debitoLocalizado: boolean = false;
+    public creditoLocalizado: boolean = false;
 
     ngOnInit(){
-      this.listarTodos()
+      //this.listarTodos()
+         this.paginaForm = this.fb.group({
+          quantPag: [ 50 ]
+        });
+
+        this.totalListaExtrato();
+        this.listarExtratoPaginado(this.pagina, this.tamanho);
     }
 
+    public totalListaExtrato(): void {
+      this.extratoService.fullList().pipe(take(1)).subscribe((res: number)=>{
+      this.totalElements = res;
+    });
+  }
 
-    public listarTodos(){
-      this.extratoService.findAll().pipe(take(1)).subscribe((res: Extrato[])=>{
-         this.extratos = res;
-         this.banco = res[0].banco;
-         this.agencia = res[0].agencia;
-         this.conta = res[0].conta;
-      });
+  public verificarDebito(valor: number, id: number, data: string): void {
+    if(valor != 0){
+     //console.log(valor, id, "D")
+
+         this.extratoService.verificaDebito(valor, id, data).pipe(take(1)).subscribe((res:any)=>{
+        this.debitoLocalizado = true
+        console.log(res)
+     });
     }
+  }
+
+  public verificarCredito(valor: number, id: number): void {
+    if(valor != 0){
+      console.log(valor, id, "C")
+      this.creditoLocalizado= false;
+    }
+
+  }
+
+
+  public listarExtratoPaginado(page: number, size: number){
+    this.extratoService.findAll(page, size).pipe(take(1)).subscribe((res: Extrato[])=>{
+        this.extratos = res;
+        //console.log(res);
+        this.banco = res[0].banco;
+        this.agencia = res[0].agencia;
+        this.conta = res[0].conta;
+        //this.carregando = true;
+        if(this.totalElements > this.paginaForm.get('quantPag')?.value){
+          this.totalPages = this.totalElements/this.paginaForm.get('quantPag')?.value;
+        } else {
+          this.totalPages = 1;
+        }
+      }
+    );
+  }
+
+    public paginaMenor(): void {
+    if(this.pagina <= 0){
+      this.pagina = 0;
+    } else {
+      this.pagina = this.pagina - 1;
+    }
+    this.listarExtratoPaginado(this.pagina, this.paginaForm.get('quantPag')?.value);
+  }
+
+  public paginaMaior(): void {
+    if(this.totalPages > 1){
+      this.pagina = this.pagina + 1;
+      this.listarExtratoPaginado(this.pagina, this.paginaForm.get('quantPag')?.value);
+    }
+  }
+
+  public atualizaPagina(): void {
+    this.pagina = 0
+    this.listarExtratoPaginado(this.pagina, this.paginaForm.get('quantPag')?.value);
+  }
 }
--- src/app/modulos/extrato/extrato.service.ts
+++ src/app/modulos/extrato/extrato.service.ts
@@ -1,6 +1,6 @@
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../../environments/environments';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Extrato } from '../../domain/extrato.domain';
 
@@ -12,12 +12,21 @@ export class ExtratoService {
   private readonly api = `${environment.api}/conciliacao/api/v1/extrato`;
   private tokenHeader: string =  environment.TOKEN;
 
-  private http = inject(HttpClient); 
+  private http = inject(HttpClient);
 
   constructor() { }
 
-  public findAll(): Observable<Extrato[]> {
-    return this.http.get<Extrato[]>(`${this.api}/listar`);
+  public fullList(): Observable<number> {
+    return this.http.get<number>(`${this.api}/totalLista`);
   }
-  
+
+  public findAll(page: number, size: number): Observable<Extrato[]> {
+    const params = new HttpParams().set('page', page).set('size', size);
+    return this.http.get<Extrato[]>(`${this.api}/listar?${params.toString()}`);
+  }
+
+  public verificaDebito(valor: number, id: number, data: string): Observable<any> {
+    return this.http.get<any>(`${this.api}/verificaDebito/${valor}/${id}/${data}`);
+  }
+
 }
